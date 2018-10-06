5f40fa65b3eace762d2beabdaea2ed7200ace468
Pervak Maria <pervak.maria@gmail.com>
2018-10-07 02:22:17 +0400

    post own slugify

--- news_portal/models.py
+++ news_portal/models.py
@@ -1,11 +1,31 @@
 from django.db import models
 from django.shortcuts import reverse
-from django.utils.text import slugify
+# from django.utils.text import slugify
 from time import time
+import re
+
+
+def slugify(s):
+    pattern = r'[^\w+]'
+    vocabulary = {'а':'a','б':'b','в':'v','г':'g','д':'d','е':'e','ё':'e',
+      'ж':'zh','з':'z','и':'i','й':'i','к':'k','л':'l','м':'m','н':'n',
+      'о':'o','п':'p','р':'r','с':'s','т':'t','у':'u','ф':'f','х':'h',
+      'ц':'c','ч':'cz','ш':'sh','щ':'scz','ъ':'','ы':'y','ь':'','э':'e',
+      'ю':'u','я':'ja', 'А':'a','Б':'b','В':'v','Г':'g','Д':'d','Е':'e','Ё':'e',
+      'Ж':'zh','З':'z','И':'i','Й':'i','К':'k','Л':'l','М':'m','Н':'n',
+      'О':'o','П':'p','Р':'r','С':'s','Т':'t','У':'u','Ф':'Х','х':'h',
+      'Ц':'c','Ч':'cz','Ш':'sh','Щ':'scz','Ъ':'','Ы':'y','Ь':'','Э':'e',
+      'Ю':'u','Я':'ja',',':'','?':'',' ':'_','~':'','!':'','@':'','#':'',
+      '$':'','%':'','^':'','&':'','*':'','(':'',')':'','-':'','=':'','+':'',
+      ':':'',';':'','<':'','>':'','\'':'','"':'','\\':'','/':'','№':'',
+      '[':'',']':'','{':'','}':'','ґ':'','ї':'', 'є':'','Ґ':'g','Ї':'i','Є':'e'}
+    for key in vocabulary:
+        s = s.replace(key, vocabulary[key])
+    return re.sub(pattern, '-', s)
 
 def gen_slug(s):
-    new_slug = slugify(s, allow_unicode=True)
-    return new_slug + '-' + str(int(time()))
+    new_slug = slugify(s)
+    return new_slug + '-' + str(int(time())) # make unique
 
 
 
